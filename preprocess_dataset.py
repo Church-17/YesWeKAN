e5e6cf802c24b999522268875e448449a9a25fde
xxFrancesco <cardiafrancesco30@gmail.com>
2024-07-02 22:28:29 +0200

    aggiunti val datasets

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -9,14 +9,15 @@ def standardize(dataset: np.ndarray) -> tf.Tensor:
         dataset[:, i] = (column - np.min(column)) / (np.max(column) - np.min(column))
     return dataset
 
-def build_model(nodes: list, input_dim: int, loss: str='mse', optimizer: str='adam', metrics: list=['accuracy'], mlp: bool=True):
+def build_model(nodes: list, input_dim: int, loss: str='mse', optimizer: str='adam', metrics: list=['mse'], mlp: bool=True):
     model = tf.keras.Sequential()
     numero_livelli = len(nodes)
 
     if mlp:
         model.add(tf.keras.layers.Input(shape=(input_dim,)))
-        for i in range(0, numero_livelli):
+        for i in range(0, numero_livelli-1):
             model.add(tf.keras.layers.Dense(nodes[i], activation='relu'))
+        model.add(tf.keras.layers.Dense(nodes[i+1], activation=None))
     else:
         model.add(tf.keras.layers.Input(shape=(input_dim,)))
         for i in range(0, numero_livelli):
